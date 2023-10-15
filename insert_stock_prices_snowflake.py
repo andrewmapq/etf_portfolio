b17af2a5498570be09bac8d85baa32b2e986aa46
andrewmapq <andrewmapq@gmail.com>
2023-10-15 20:54:32 -0300

    fixing missing tickers

--- insert_stock_prices_snowflake.py
+++ insert_stock_prices_snowflake.py
@@ -9,51 +9,69 @@ from logging_config import configure_logging
 configure_logging()
 yf.pdr_override()
 
-# Define your ODBC connection string
-snowflake_username = 'andrewmapq'
-snowflake_password = 'Thom@sS@nkara29'
-snowflake_account = 'cpqcihe-to04558'
-snowflake_database = 'ETF_CONSTITUENT_DATA'
-snowflake_schema = 'public'
-snowflake_warehouse = 'compute_wh'
+def change_tickers(df_stocks):
+    data_change = {'constituent_ticker': ['FB', 'FISV','MRVL.UW','TCOM.UW'],
+            'chg_ticker': ['META', 'FI','MRVL','TCOM']}
+    df_change = pd.DataFrame(data_change)
 
-snowflake_password_encoded = quote(snowflake_password)
+    merged_df = df_stocks.merge(df_change, on='constituent_ticker', how='left')
 
-# Create an SQLAlchemy engine
-engine = create_engine(
-    f'snowflake://{snowflake_username}:{snowflake_password_encoded}@{snowflake_account}.snowflakecomputing.com/{snowflake_database}/{snowflake_schema}',
-    connect_args={
-        'account': snowflake_account,
-        'warehouse': snowflake_warehouse,
-    },
-    echo=False  # Enable echo for debugging SQL statements (optional)
-)
+    df_stocks['constituent_ticker'] = merged_df['chg_ticker'].combine_first(df_stocks['constituent_ticker'])
 
-logging.info('Connected to snowflake')
+    logging.info(df_stocks)
+    return df_stocks
 
-query = "select CONSTITUENT_TICKER from CONSTITUENTS where composite_ticker = 'QQQ' and etfg_date = '2021-11-1' order by WEIGHT desc;"
+def get_engine_db():
+    snowflake_username = 'andrewmapq'
+    snowflake_password = 'Thom@sS@nkara29'
+    snowflake_account = 'cpqcihe-to04558'
+    snowflake_database = 'ETF_CONSTITUENT_DATA'
+    snowflake_schema = 'public'
+    snowflake_warehouse = 'compute_wh'
 
-# Use pandas to read data from the database
-stocks = pd.read_sql_query(query, con=engine)
+    snowflake_password_encoded = quote(snowflake_password)
 
-logging.info(stocks)
+    engine = create_engine(
+        f'snowflake://{snowflake_username}:{snowflake_password_encoded}@{snowflake_account}.snowflakecomputing.com/{snowflake_database}/{snowflake_schema}',
+        connect_args={
+            'account': snowflake_account,
+            'warehouse': snowflake_warehouse,
+        },
+        echo=False  # Enable echo for debugging SQL statements (optional)
+    )
+    logging.info('Connected to snowflake')
 
-# Create an empty list to store DataFrames
-dfs = []
+    return engine
 
-for stock in stocks['constituent_ticker'].values:
-    logging.info(f'Getting prices for the stock :{stock}')
-    # download dataframe
-    df_stocks = pdr.get_data_yahoo(stock, start="2023-10-01", end="2023-10-13")
-    df_stocks["Ticker"] = stock
+def main():
+    # Get connection string
+    engine = get_engine_db()
 
-    dfs.append(df_stocks)
+    query = "select CONSTITUENT_TICKER from CONSTITUENTS where composite_ticker = 'QQQ' and etfg_date = '2021-11-1' order by WEIGHT desc;"
+    stocks = pd.read_sql_query(query, con=engine)
+    logging.info(stocks)
+    stocks = change_tickers(stocks)
 
-# Concatenate all DataFrames into a single DataFrame
-merged_df = pd.concat(dfs)
+    # Create an empty list to store DataFrames
+    dfs = []
 
-# Reset the index of the merged DataFrame
-merged_df.reset_index(inplace=True)
+    for stock in stocks['constituent_ticker'].values:
+        logging.info(f'Getting prices for the stock :{stock}')
+        # download dataframe
+        df_stocks = pdr.get_data_yahoo(stock, start="2023-10-01", end="2023-10-13")
+        df_stocks["Ticker"] = stock
 
-# Display the merged DataFrame
-print(merged_df)
+        dfs.append(df_stocks)
+
+    # Concatenate all DataFrames into a single DataFrame
+    merged_df = pd.concat(dfs)
+
+    # Reset the index of the merged DataFrame
+    merged_df.reset_index(inplace=True)
+
+    # Display the merged DataFrame
+    print(merged_df)
+
+
+if __name__ == "__main__":
+    main()
